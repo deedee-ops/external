b5b83eac4e20e96f7155507067a4faf872a09b2c
Igor Rzegocki <igor@rzegocki.pl>
2025-03-13 20:21:50 +0100

    chore: temporary disable rustdesk relay domain

--- cloudflare/records.tf
+++ cloudflare/records.tf
@@ -9,11 +9,11 @@ resource "cloudflare_record" "external_ingress" {
 }
 
 # rustdesk relay
-resource "cloudflare_record" "rustdesk_relay" {
-  zone_id = data.cloudflare_zone.base_domain.id
-  name    = "relay"
-  content = "homelab.${data.cloudflare_zone.base_domain.name}"
-  type    = "CNAME"
-  proxied = false
-  ttl     = 1
-}
+# resource "cloudflare_record" "rustdesk_relay" {
+#   zone_id = data.cloudflare_zone.base_domain.id
+#   name    = "relay"
+#   content = "homelab.${data.cloudflare_zone.base_domain.name}"
+#   type    = "CNAME"
+#   proxied = false
+#   ttl     = 1
+# }
